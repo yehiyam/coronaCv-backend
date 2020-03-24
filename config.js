b80450dbe408cee6235f5b8b09beadcfbd0a7657
Yehiyam Livneh <yehiyam@gmail.com>
2020-03-24 18:50:41 +0200

    updates

--- config.js
+++ config.js
@@ -2,6 +2,11 @@ const config = {
     PORT: process.env.PORT || 3000,
     dbConfig: {
         connection: process.env.DB_CONNECTION || 'mongodb://localhost/test'
+    },
+    cvConfig:{
+        host: process.env.CVMONITOR_HOST || 'localhost',        
+        port: process.env.CVMONITOR_PORT || '8080',     
+        schema: 'http'   
     }
 }
 
