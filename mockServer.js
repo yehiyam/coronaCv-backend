d8f9748e4ccb66f8165109d4fcc3ade1bd94473c
Ubuntu <rambammonitors@rambammonitorscv.1wy5hsbhgczulj5nos4rjrk4ra.ax.internal.cloudapp.net>
2020-04-22 16:29:04 +0000

    add mapping for coview

--- mockServer.js
+++ mockServer.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080
 app.use(bodyParser.text({type: '*/*'}))
 app.post('/', (req, res) => {
     console.log(req.body)
-    res.json({ status: 'Ok' });
+    res.send('ok');
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
