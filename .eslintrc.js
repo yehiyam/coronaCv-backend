18ec1ee04afd85e8687d58ba5c60b4ac30c25975
Yehiyam Livneh <yehiyam@gmail.com>
2020-04-27 14:20:00 +0300

    fix linting

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'no-console': 'off',
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': 'off',
+    'max-len': ['error', { code: 130 }],
+    'import/newline-after-import': 'off'
+  },
+};
